87064bf500f8a3b7e361d37e082b9686ab239cb5
guptariya <riyagupta@tristonsoft.com>
2019-01-08 12:28:57 +0530

    html file fields added to table

--- pdf_html/admin.py
+++ pdf_html/admin.py
@@ -1,2 +1,5 @@
 from django.contrib import admin
- 
+from .models import Profile, patient_profile_conclusion
+
+admin.site.register(Profile)
+admin.site.register(patient_profile_conclusion)
--- pdf_html/models.py
+++ pdf_html/models.py
@@ -4,5 +4,17 @@ class Profile(models.Model):
    name = models.CharField(max_length = 50)
    file = models.FileField(upload_to = 'media')
 
-   class Meta:
-      db_table = "media"
+   def __unicode__(self):  
+        return self.name
+   
+class patient_profile_conclusion(models.Model):
+   patient_name = models.CharField(max_length = 50,default="")
+   hemoglobin_cnt = models.CharField(max_length = 5,default="0")
+   pcv_cnt= models.CharField(max_length = 5,default="0")
+   RBC_cnt=models.CharField(max_length = 5,default="0")
+   MCV_cnt=models.CharField(max_length = 5,default="0")
+   MCH_cnt=models.CharField(max_length = 5,default="0")
+   
+   def __unicode__(self):  
+        return self.patient_name
+   
